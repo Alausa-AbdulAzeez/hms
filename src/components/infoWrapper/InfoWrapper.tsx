a11f4917c08d436748a7adc625a3e085ae550ff9
unknown <Alausa-AbdulAzeez>
2022-12-24 16:13:22 +0100

    adjust responsiveness

--- src/components/infoWrapper/InfoWrapper.tsx
+++ src/components/infoWrapper/InfoWrapper.tsx
@@ -1,41 +1,41 @@
-import 'react-calendar/dist/Calendar.css'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import "react-calendar/dist/Calendar.css";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
-import './infoWrapper.css'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import "./infoWrapper.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-import NoticeBoard from '../noticeBoard/NoticeBoard'
+import NoticeBoard from "../noticeBoard/NoticeBoard";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
 const InfoWrapper = () => {
   const myEventsList = [
     {
       start: moment().toDate(),
       end: moment().toDate(),
-      title: 'Some title avhjk cvlJ',
+      title: "Some title avhjk cvlJ",
     },
-  ]
+  ];
   return (
-    <div className='infoWrapper'>
-      <div className='calendar'>
+    <div className="infoWrapper">
+      <div className="calendar">
         <Calendar
           localizer={localizer}
           events={myEventsList}
-          startAccessor='start'
-          endAccessor='end'
+          startAccessor="start"
+          endAccessor="end"
           style={{
-            height: 600,
-            width: 700,
-            zIndex: 0,
-            backgroundColor: 'aliceblue',
+            // height: 600,
+            // width: 500,
+            // zIndex: 0,
+            backgroundColor: "aliceblue",
           }}
         />
       </div>
       <NoticeBoard />
     </div>
-  )
-}
+  );
+};
 
-export default InfoWrapper
+export default InfoWrapper;
